f443ab084b2e13fffc2eb05bbfe1d05fdf6d8a7b
larnicol <larnicol.titouan@orange.fr>
2016-06-03 14:15:27 +0200

    tamere

---
+++ PLA/src/automate.java
@@ -0,0 +1,19 @@
+
+public class automate {
+
+	private Etat initiale;
+	
+	
+	public static void main(String[] args) {
+		
+		
+		automate(){
+			this.initiale=new Etat();
+			
+			
+		}
+		
+
+	}
+
+}
