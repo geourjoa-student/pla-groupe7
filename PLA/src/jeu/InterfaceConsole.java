662589c71e4b9664d69b974b2f6fad8b2f14fc34
clement.rouquier <clement.rouquier@e.ujf-grenoble.fr>
2016-06-07 09:02:00 +0200

    Petite correction InterfaceConsole

--- PLA/src/jeu/InterfaceConsole.java
+++ PLA/src/jeu/InterfaceConsole.java
@@ -5,9 +5,7 @@ public class InterfaceConsole extends InterfaceUtilisateur {
 	@Override
 	public void afficherMap(int[][] decor) {
 		
-		int i,j,k = 0;
 		for (int i = 0; i < decor.length; i++) {
-			afficheSeparateur(j);
 			for (int j = 0; j < decor[i].length; j++) {
 				System.out.print("|");
 				System.out.format("%3d",decor[i][j]);
