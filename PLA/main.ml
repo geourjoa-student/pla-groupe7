1d36716ac2d30331814d9d64748223d144c8e40b
rocher <rocher@rocher-HP-ENVY-m6-Notebook-PC>
2016-06-10 15:18:18 +0200

    fichiers caml et xml des 6 automates

--- PLA/main.ml
+++ PLA/main.ml
@@ -4,195 +4,241 @@ type cellule =
   | C (* case *)| N (* nord *) | S | E | O
 
 type action =
-  | Attendre
+  | Ne_rien_faire
   | Deplacer of cellule
-  | Attaquer of cellule
-  | Cueillir
-  | Couper
-  | Convertir of cellule
-  | Soigner of cellule
+  | Attaquer
+  | Recolter
+  | Soigner
+  | Convertir
+  | Creer_unite
+  | Restaurer_case
 
 type condition =
-  | Herbe of cellule
-  | Allie of cellule
-  | Ennemi of cellule
-  | Nourriture of cellule
-  | Bois of cellule
+  | Aucune_condition
+  | Ennemi_adjacent
+  | Allie_adjacent
+  | Ressource_adjacente
+  | Ressource_sous_case
+  | Caserne_sous_case
+  | Eglise_sous_case
+  | Ferme_sous_case
+  | Batiment_ennemi_adjacent
+
+type role =
+  |Guerrier
+  |Moine
+  |Paysan
+  |Heros
+
+type joueur = JOUEUR1 | JOUEUR2
 
 type etat = int
 type priorite = int
 type transition = etat * condition * priorite * action * etat
-type automate = transition list * etat (*etat initial*)
-
-(* EXEMPLE D'AUTOMATE *)
-
-let paysan =
-	[(1, Nourriture(N), Deplacer(N), 1);
-	(1, Nourriture(S), Deplacer(S), 1);
-	(1, Nourriture(E), Deplacer(E), 1);
-	(1, Nourriture(O), Deplacer(O), 1);
-	(1, Nourriture(C), Cueillir, 1);
-	(1, Bois(N), Deplacer(N), 1);
-	(1, Bois(S), Deplacer(S), 1);
-	(1, Bois(E), Deplacer(E), 1);
-	(1, Bois(O), Deplacer(O), 1);
-	(1, Bois(C), Couper, 1);
-	(1, Herbe(C), Deplacer(N), 1); (* déplacement dans une direction aléatoire *)
-	(1, Herbe(C), Deplacer(S), 1);
-	(1, Herbe(C), Deplacer(O), 1);
-	(1, Herbe(C), Deplacer(E), 1);
-	
-	];;
-	
-	let guerrier =
-	[(1, Ennemi(N), Attaquer(N), 1);
-	(1, Ennemi(S), Attaquer(S), 1);
-	(1, Ennemi(E), Attaquer(E), 1);
-	(1, Ennemi(O), Attaquer(O), 1);
-	(1, Herbe(C), Deplacer(N), 1); (* déplacement dans une direction aléatoire *)
-	(1, Herbe(C), Deplacer(S), 1);
-	(1, Herbe(C), Deplacer(O), 1);
-	(1, Herbe(C), Deplacer(E), 1);
-	];;
-
-	let moine =
-	[(1, Ennemi(N), Convertir(N), 1);
-	(1, Ennemi(S), Convertir(S), 1);
-	(1, Ennemi(E), Convertir(E), 1);
-	(1, Ennemi(O), Convertir(O), 1);
-	(1, Allie(N), Soigner(N), 1);
-	(1, Allie(S), Soigner(S), 1);
-	(1, Allie(E), Soigner(E), 1);
-	(1, Allie(O), Soigner(O), 1);
-	(1, Herbe(C), Deplacer(N), 1); (* déplacement dans une direction aléatoire *)
-	(1, Herbe(C), Deplacer(S), 1);
-	(1, Herbe(C), Deplacer(O), 1);
-	(1, Herbe(C), Deplacer(E), 1);
-	];;
-
-    
-   
-(* ON PEUT GÉNÉRER CERTAINES PARTIES DE L'AUTOMATE *)
-
-let attaquerNSEO depart arrivee  =
-      List.map  (fun direction -> (depart, Ennemi(direction), Attaquer(direction), arrivee) ) [N;S;E;O];;
-
-let guerrier2 =
-  (attaquerNSEO 1 1) 
-    @
-  [(1, Herbe(C), Deplacer(N), 1); (* déplacement dans une direction aléatoire *)
-	(1, Herbe(C), Deplacer(S), 1);
-	(1, Herbe(C), Deplacer(O), 1);
-	(1, Herbe(C), Deplacer(E), 1);
-	];;
+type automate = joueur * role * transition list * etat (*etat initial*)
 
 
-(* Traduction du tableau d'entiers en fichier xml *)
+(* TRADUCTION DES CONDITIONS ET ACTION EN ENTIERS *)
+
+let joueur_to_int joueur = match joueur with
+  | JOUEUR1 -> 1
+  | JOUEUR2 -> 2
+;;
+
+let role_to_int role = match role with
+  |Guerrier -> 1
+  |Moine -> 2
+  |Paysan -> 3
+  |Heros -> 4
+;;
+
+let condition_to_int condition = match condition with
+  | Aucune_condition -> 0
+  | Ennemi_adjacent -> 1
+  | Allie_adjacent -> 2
+  | Ressource_adjacente -> 3
+  | Ressource_sous_case -> 4
+  | Caserne_sous_case -> 5
+  | Eglise_sous_case -> 6
+  | Ferme_sous_case -> 7
+  | Batiment_ennemi_adjacent -> 8
+;;
+
+
+let action_to_int action = match action with
+  | Ne_rien_faire -> 0
+  | Deplacer(O) -> 1
+  | Deplacer(E) -> 2
+  | Deplacer(N) -> 3
+  | Deplacer(S) -> 4
+  | Attaquer -> 6
+  | Recolter -> 7
+  | Soigner -> 9
+  | Convertir -> 10
+  | Creer_unite -> 11
+  | Restaurer_case -> 12
+  | _ -> -1
+
+let transition_to_ints transition = match transition with
+  |(depart, condition, priorite, action, arrivee) -> (depart, condition_to_int condition, priorite, action_to_int action, arrivee);; 
+
+let rec transitions_to_ints transitions = match transitions with
+  |t::q -> (transition_to_ints t) :: (transitions_to_ints q)
+  |[] -> [];;
+
+let rec valider_action_paysan action = match action with
+  |Deplacer(cel) -> Deplacer(cel)
+  |Attaquer -> Attaquer (*les paysans peuvent attaquer de base*)
+  |Recolter -> Recolter
+  |_ -> Ne_rien_faire;; (* si l'action n'est pas valide alors on la remplace par attendre*)
+
+let rec valider_action_guerrier action = match action with
+  |Deplacer(cel) -> Deplacer(cel)
+  |Attaquer -> Attaquer
+  |_ -> Ne_rien_faire;;
+
+let rec valider_action_moine action = match action with
+  |Deplacer(cel) -> Deplacer(cel)
+  |Convertir -> Convertir
+  |Soigner -> Soigner
+  |_ -> Ne_rien_faire;;
+
+let valider_action action role = match role with
+  |Guerrier -> valider_action_guerrier action
+  |Paysan -> valider_action_paysan action
+  |Moine -> valider_action_moine action
+  |Heros -> action;;
+
+
+let rec valider_transitions transitions role = match transitions with
+  |(depart, condition, priorite, action, arrivee) :: q -> (depart, condition, priorite, valider_action action role, arrivee) :: (valider_transitions q role) 
+  |[] -> [];;
+
+let valider_automate automate = match automate with
+  |(joueur, role, transitions, etat) ->  (joueur, role, valider_transitions transitions role, etat);;
+
+
+
+(* EXEMPLEs D'AUTOMATE *)
+
+let paysan_joueur1 = 
+
+	(JOUEUR1, (* le propriétaire de l'automate*)
+
+	Paysan, (* le rôle de l'automate *)
+
+	[(1, Ressource_adjacente, 1, Recolter, 1); (* la liste des transitions *)
+	(1, Ressource_sous_case, 2, Recolter, 1);
+	(1, Aucune_condition, 1, Deplacer(N), 1); (* déplacement dans une direction aléatoire *)
+	(1, Aucune_condition, 1,  Deplacer(S), 1);
+	(1, Aucune_condition, 1, Deplacer(O), 1);
+	(1, Aucune_condition, 1, Deplacer(E), 1);],
+
+    1) (*l'état final*);;
+
+let guerrier_joueur1 = 
+	(JOUEUR1,
+	Guerrier,
+	[(1, Ennemi_adjacent, 1, Attaquer, 1);
+	(1, Aucune_condition, 1, Deplacer(N), 1); 
+	(1, Aucune_condition, 1,  Deplacer(S), 1);
+	(1, Aucune_condition, 1, Deplacer(O), 1);
+	(1, Aucune_condition, 1, Deplacer(E), 1);],
+    1);;
+
+let moine_joueur1 = 
+	(JOUEUR1,
+	Moine,
+	[(1, Ennemi_adjacent, 1, Convertir, 1);
+	(1, Allie_adjacent, 1, Soigner, 1);
+	(1, Aucune_condition, 1, Deplacer(N), 1); 
+	(1, Aucune_condition, 1,  Deplacer(S), 1);
+	(1, Aucune_condition, 1, Deplacer(O), 1);
+	(1, Aucune_condition, 1, Deplacer(E), 1);],
+    1);;
+
+let paysan_joueur2 = 
+	(JOUEUR1,
+	Paysan,
+	[(1, Ressource_adjacente, 1, Recolter, 1);
+	(1, Ressource_sous_case, 2, Recolter, 1);
+	(1, Aucune_condition, 1, Deplacer(N), 1); 
+	(1, Aucune_condition, 1,  Deplacer(S), 1);
+	(1, Aucune_condition, 1, Deplacer(O), 1);
+	(1, Aucune_condition, 1, Deplacer(E), 1);],
+    1);;
+
+
+let guerrier_joueur2 = 
+	(JOUEUR1,
+	Guerrier,
+	[(1, Ennemi_adjacent, 1, Attaquer, 1);
+	(1, Aucune_condition, 1, Deplacer(N), 1); 
+	(1, Aucune_condition, 1,  Deplacer(S), 1);
+	(1, Aucune_condition, 1, Deplacer(O), 1);
+	(1, Aucune_condition, 1, Deplacer(E), 1);],
+    1);;
+
+let moine_joueur2 = 
+	(JOUEUR1,
+	Moine,
+	[(1, Ennemi_adjacent, 1, Convertir, 1);
+	(1, Allie_adjacent, 1, Soigner, 1);
+	(1, Aucune_condition, 1, Deplacer(N), 1); 
+	(1, Aucune_condition, 1,  Deplacer(S), 1);
+	(1, Aucune_condition, 1, Deplacer(O), 1);
+	(1, Aucune_condition, 1, Deplacer(E), 1);],
+    1);;
+
+(* Traduction vers un fichier xml *)
 
 let rec transitions_to_xml liste_transitions fichier = match liste_transitions with
 |[]-> ()
-|(depart, condition, priorite, action, arrivee)::q -> output_string fichier "<transition>";
-											output_string fichier "<depart>";
-											output_string fichier (string_of_int depart);
-											output_string fichier "</depart>";
-											output_string fichier "<condition>";
-											output_string fichier (string_of_int condition);
-											output_string fichier "</condition>";
-											output_string fichier "<priorite>";
-											output_string fichier (string_of_int priorite);
-											output_string fichier "</priorite>";
-											output_string fichier "<action>";
-											output_string fichier (string_of_int action);
-											output_string fichier "</action>";
-											output_string fichier "<arrivee>";
-											output_string fichier (string_of_int arrivee);
-											output_string fichier "</arrivee>";
-											output_string fichier "</transition>";
-											transitions_to_xml q fichier;;
+|(depart, condition, priorite, action, arrivee)::q -> 
+	output_string fichier "<transition>";
+	output_string fichier "<depart>";
+	output_string fichier (string_of_int depart);
+	output_string fichier "</depart>";
+	output_string fichier "<condition>";
+	output_string fichier (string_of_int condition);
+	output_string fichier "</condition>";
+	output_string fichier "<priorite>";
+	output_string fichier (string_of_int priorite);
+	output_string fichier "</priorite>";
+	output_string fichier "<action>";
+	output_string fichier (string_of_int action);
+	output_string fichier "</action>";
+	output_string fichier "<arrivee>";
+	output_string fichier (string_of_int arrivee);
+	output_string fichier "</arrivee>";
+	output_string fichier "</transition>";
+	transitions_to_xml q fichier;;
+
+
+let automate_to_xml automate fichier = match automate with
+  |(joueur, role, transitions, etat) -> 
+  	output_string fichier "<automate type=\"";
+  	output_string fichier (string_of_int (role_to_int role));
+  	output_string fichier "\">";
+  	(transitions_to_xml (transitions_to_ints transitions) fichier);
+  	output_string fichier "</automate>";;
+
 
 let fic = open_out "automates.xml";;
-output_string fic "<automate>";;
-transitions_to_xml [(1,2,3,4,6);(5,6,7,8,9)] fic;;
-output_string fic "</automate>";;
 
+output_string fic "<partie>";
 
-   
-(* TRADUCTION DES CONDITIONS COMPLEXES EN ENTIER *)
+output_string fic "<joueur id = \"1\" >";
+automate_to_xml guerrier_joueur1 fic;
+automate_to_xml moine_joueur1 fic;
+automate_to_xml paysan_joueur1 fic;
+output_string fic "</joueur>";
+
+output_string fic "<joueur id = \"2\" >";
+automate_to_xml guerrier_joueur2 fic;
+automate_to_xml moine_joueur2 fic;
+automate_to_xml paysan_joueur2 fic;
+output_string fic "</joueur>";
+
+output_string fic "</partie>";;
   
-let (cellule_to_int: cellule -> int) = function
-  | C -> 0
-  | N -> 1
-  | S -> 2
-  | E -> 3
-  | O -> 4
-  
-  (*
-
-let (condition_to_int: condition -> int) =  function
-  | Vide -> 0
-  | Ami(cellule) -> 1 + (cellule_to_int cellule) (* 1..5 *)
-  | Ennemi(cellule) -> 6 + (cellule_to_int cellule) (* 6..10 *)
-  | Comestible(cellule) -> 11 + (cellule_to_int cellule) (* 11..15 *)
-  | _ -> 0
-
-let (action_to_int: action -> int) = function
-   | Attendre -> 0
-   | Avancer -> 1
-   | Reculer -> 2
-   | Frapper -> 3
-   | Prendre -> 4
-   | Tourner_vers (cellule) -> 5 + (cellule_to_int cellule)
-   | _ -> 0
-
-   
-   
-  
-let (traduction_transition: transition -> int * int * int * int) = fun (src,condition,action,tgt) ->
-   (src, condition_to_int condition, action_to_int action, tgt)
-
-let (traduction_automate: automate -> (int * int * int * int) list) = fun automate ->
-   List.map traduction_transition automate ;;
-
-
-let trad_aut1 = traduction_automate aut1 ;;
-
-(* On obtient
-   [ (1, 7, 6, 2); 
-     (1, 8, 7, 2); 
-     (1, 9, 8, 2); 
-     (1, 10, 9, 2); 
-     (2, 7, 3, 3);
-     (3, 0, 1, 4); 
-     (4, 0, 0, 1) ]
-
-  à partir duquel on peut constuire le tableau des transitions et celui des actions.
-
- *)
-
-   
-(* LE SIMULATEUR examine le voisinage de la position (x,y) du personnage 
-   
-         ?      |  Ennemi(N)    |  ?
- ------------------------------------------------
-  Comestible(O) | Comestible(C) | Comestible(E)
- ------------------------------------------------
-         ?      |  Ennemi(S)    |  ?
-
-qui correspond aux conditions 
-
-    ?  | 7  | ?
-    15 | 11 | 14
-     ? | 8  | ? 
-
-Supposons que l'automate du personnage soit dans l'état 1,
-le simulateur cherche les transitions exécutables de l'automate 
-dans l'état 1 sur les conditions/symboles {7,8,11,14,15}
-il y en a deux transitions possibles :
-
- (1, 7, 6, 2)  et (1, 8, 7, 2);
-
-Le simulateur en prend tire une parmi celle là est l'exécute.
-
-
-*)
-*)
